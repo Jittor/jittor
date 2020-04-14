43279ae58fe1a2f6051a725b82e61b5cfa949854
guowei yang <Gword>
2020-04-14 15:35:15 +0800

    test mpi batchnorm

--- python/jittor/test/test_mpi_batchnorm.py
+++ python/jittor/test/test_mpi_batchnorm.py
@@ -9,47 +9,31 @@
 import unittest
 import os, sys
 import jittor as jt
+from jittor import nn
 import numpy as np
 
-def test_all_reduce():
-    print("test all_reduce")
-    x = jt.random([5, 5])
-    y = jt.compile_extern.mpi_ops.mpi_all_reduce(x)
-    assert np.allclose(y.data, (x*3).data)
-
-def test_broadcast():
-    print("test broadcast")
+def test_batchnorm():
+    print("test batchnorm")
     mpi = jt.compile_extern.mpi
-    data = jt.random([5, 5])
-    if mpi.world_rank() == 0:
-        x = data
-    else:
-        x = jt.zeros([5, 5])
-    y = jt.compile_extern.mpi_ops.mpi_broadcast(x, 0)
-    assert np.allclose(y.data, data.data)
+    data = np.random.rand(30,3,10,10)
+    x1 = jt.array(data)
+    x2 = jt.array(data[mpi.world_rank()*10:(mpi.world_rank()+1)*10,...])
+    
+    bn1 = nn.BatchNorm(3)
+    bn2 = nn.SyncBatchNorm(3)
+    y1 = bn1(x1).data
+    y2 = bn2(x2).data
 
-def test_reduce():
-    print("test reduce")
-    mpi = jt.compile_extern.mpi
-    x = jt.random([5, 5])
-    y = jt.compile_extern.mpi_ops.mpi_reduce(x, 0)
-    y.sync()
-    if mpi.world_rank() == 0:
-        assert np.allclose(y.data, (x*3).data)
+    assert bn1.running_mean==bn2.running_mean
+    assert bn1.running_var==bn2.running_var
 
 def main():
     np.random.seed(0)
     jt.set_seed(3)
     with jt.flag_scope(use_cuda=0):
-        if jt.compile_extern.mpi_ops:
-            test_all_reduce()
-            test_broadcast()
-            test_reduce()
+        test_batchnorm()
     with jt.flag_scope(use_cuda=1):
-        if jt.compile_extern.mpi_ops:
-            test_all_reduce()
-            test_broadcast()
-            test_reduce()
+        test_batchnorm()
 
 @unittest.skipIf(not jt.compile_extern.has_mpi, "no mpi found")
 class TestMpiOps(unittest.TestCase):
@@ -57,7 +41,7 @@ class TestMpiOps(unittest.TestCase):
         mpi = jt.compile_extern.mpi
         if not jt.compile_extern.inside_mpi():
             mpirun_path = jt.compiler.env_or_try_find('mpirun_path', 'mpirun')
-            cmd = f"{mpirun_path} -np 3 {sys.executable} -m jittor.test.test_mpi_op"
+            cmd = f"{mpirun_path} -np 3 {sys.executable} -m jittor.test.test_mpi_batchnorm"
             print("run cmd", cmd)
             jt.compiler.run_cmd(cmd)
         else:
