5b7e057f6c62cf2438c448cb2b465b9ef3db5f22
Dun Liang <randonlang@gmail.com>
2020-04-16 15:53:05 +0800

    add cos/sin/tan[ah] and fix tanh nan #61

--- src/ops/unary_op_defs.h
+++ src/ops/unary_op_defs.h
@@ -11,13 +11,30 @@ namespace jittor {
 #define bitwise_not(T,x) (~(x))
 #define negative(T,x) (-(x))
 #ifdef JIT_cuda
+// TODO: add float64 version
 #define abs(T,x) ::abs(x)
-#define log(T,x) ::log((T)(x))
-#define exp(T,x) ::exp((T)(x))
-#define sqrt(T,x) ::sqrt((T)(x))
+#define log(T,x) ::logf((T)(x))
+#define exp(T,x) ::expf((T)(x))
+#define sqrt(T,x) ::sqrtf((T)(x))
 #define round(T,x) ((T) ::roundf((x)))
 #define floor(T,x) ((T) ::floorf((x)))
 #define ceil(T,x) ((T) ::ceilf((x)))
+
+#define sin(T,x) ((T) ::sinf((x)))
+#define asin(T,x) ((T) ::asinf((x)))
+#define sinh(T,x) ((T) ::sinhf((x)))
+#define asinh(T,x) ((T) ::asinhf((x)))
+
+#define cos(T,x) ((T) ::cosf((x)))
+#define acos(T,x) ((T) ::acosf((x)))
+#define cosh(T,x) ((T) ::coshf((x)))
+#define acosh(T,x) ((T) ::acoshf((x)))
+
+#define tan(T,x) ((T) ::tanf((x)))
+#define atan(T,x) ((T) ::atanf((x)))
+#define tanh(T,x) ((T) ::tanhf((x)))
+#define atanh(T,x) ((T) ::atanhf((x)))
+
 #else
 #define abs(T,x) std::abs(x)
 #define log(T,x) std::log((T)(x))
@@ -26,7 +43,24 @@ namespace jittor {
 #define round(T,x) ((T)std::round((x)))
 #define floor(T,x) ((T)std::floor((x)))
 #define ceil(T,x) ((T)std::ceil((x)))
+
+#define sin(T,x) ((T) std::sin((x)))
+#define asin(T,x) ((T) std::asin((x)))
+#define sinh(T,x) ((T) std::sinh((x)))
+#define asinh(T,x) ((T) std::asinh((x)))
+
+#define cos(T,x) ((T) std::cos((x)))
+#define acos(T,x) ((T) std::acos((x)))
+#define cosh(T,x) ((T) std::cosh((x)))
+#define acosh(T,x) ((T) std::acosh((x)))
+
+#define tan(T,x) ((T) std::tan((x)))
+#define atan(T,x) ((T) std::atan((x)))
+#define tanh(T,x) ((T) std::tanh((x)))
+#define atanh(T,x) ((T) std::atanh((x)))
+
 #endif
+
 #define cast(T,x) ((T)(x))
 
 } // jittor
