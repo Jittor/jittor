ad320ed2cf577b45633b229d634f081b283a0286
Dun Liang <randonlang@gmail.com>
2020-07-22 22:41:17 +0800

    update version

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -7,7 +7,7 @@
 # This file is subject to the terms and conditions defined in
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
-__version__ = '1.1.6.2'
+__version__ = '1.1.6.3'
 from . import lock
 with lock.lock_scope():
     from . import compiler
