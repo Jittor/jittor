d83389117fd026a0881dd713e658ce5ae2a75bcb
Dun Liang <randonlang@gmail.com>
2020-07-27 22:26:37 +0800

    fix multi output code op not use segfault

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -7,7 +7,7 @@
 # This file is subject to the terms and conditions defined in
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
-__version__ = '1.1.6.6'
+__version__ = '1.1.6.7'
 from . import lock
 with lock.lock_scope():
     from . import compiler
