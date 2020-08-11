e25a8ac28dd1f32e0f27877effd7aeb5a45b724a
Dun Liang <randonlang@gmail.com>
2020-08-11 19:53:48 +0800

    fix numpy code op test

--- python/jittor/test/test_numpy_code_op.py
+++ python/jittor/test/test_numpy_code_op.py
@@ -58,9 +58,9 @@ class TestCodeOp(unittest.TestCase):
             one=numpy.ones(a.shape)
             assert numpy.allclose(da.data,one*2.0)
 
-        jt.flags.use_cuda = 0
-        check()
-        jt.flags.use_cuda = 1
+        if jt.has_cuda:
+            with jt.flag_scope(use_cuda=1):
+                check()
         check()
 
     def test(self):
@@ -92,9 +92,9 @@ class TestCodeOp(unittest.TestCase):
             one=numpy.ones(a.shape)
             assert numpy.allclose(da.data,one*2.0)
 
-        jt.flags.use_cuda = 0
-        check()
-        jt.flags.use_cuda = 1
+        if jt.has_cuda:
+            with jt.flag_scope(use_cuda=1):
+                check()
         check()
 
     def test_multi_input(self):
@@ -139,9 +139,9 @@ class TestCodeOp(unittest.TestCase):
             assert numpy.allclose(dda.data,one)
             assert numpy.allclose(ddb.data,mone)
         
-        jt.flags.use_cuda = 0
-        check()
-        jt.flags.use_cuda = 1
+        if jt.has_cuda:
+            with jt.flag_scope(use_cuda=1):
+                check()
         check()
 
     @unittest.skipIf(True, "Memory leak testing is not in progress, Skip")
