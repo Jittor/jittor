657a60653004c31f607cfcfdd90912601c1a61bf
li-xl <1905692338@qq.com>
2020-09-03 20:17:14 +0800

    fix affine param at Norm Module

--- python/jittor/nn.py
+++ python/jittor/nn.py
@@ -284,16 +284,16 @@ class Linear(Module):
         return x
 
 class BatchNorm(Module):
-    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=None, is_train=True, sync=True):
-        assert affine == None
-
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True, is_train=True, sync=True):
         self.sync = sync
         self.num_features = num_features
         self.is_train = is_train
         self.eps = eps
         self.momentum = momentum
-        self.weight = init.constant((num_features,), "float32", 1.0)
-        self.bias = init.constant((num_features,), "float32", 0.0)
+        self.affine = affine
+        if affine:
+            self.weight = init.constant((num_features,), "float32", 1.0)
+            self.bias = init.constant((num_features,), "float32", 0.0)
         self.running_mean = init.constant((num_features,), "float32", 0.0).stop_grad()
         self.running_var = init.constant((num_features,), "float32", 1.0).stop_grad()
 
@@ -315,20 +315,23 @@ class BatchNorm(Module):
             running_mean = self.running_mean.broadcast(x, [0,2,3])
             running_var = self.running_var.broadcast(x, [0,2,3])
             norm_x = (x-running_mean)/jt.sqrt(running_var+self.eps)
+        if not self.affine:
+            return norm_x
         w = self.weight.broadcast(x, [0,2,3])
         b = self.bias.broadcast(x, [0,2,3])
         return norm_x * w + b
         
 class BatchNorm1d(Module):
-    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=None, is_train=True, sync=True):
-        assert affine == None
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True, is_train=True, sync=True):
         self.sync = sync
         self.num_features = num_features
         self.is_train = is_train
         self.eps = eps
         self.momentum = momentum
-        self.weight = init.constant((num_features,), "float32", 1.0)
-        self.bias = init.constant((num_features,), "float32", 0.0)
+        self.affine = affine
+        if self.affine:
+            self.weight = init.constant((num_features,), "float32", 1.0)
+            self.bias = init.constant((num_features,), "float32", 0.0)
         self.running_mean = init.constant((num_features,), "float32", 0.0).stop_grad()
         self.running_var = init.constant((num_features,), "float32", 1.0).stop_grad()
 
@@ -351,20 +354,23 @@ class BatchNorm1d(Module):
             running_mean = self.running_mean.broadcast(x, [0])
             running_var = self.running_var.broadcast(x, [0])
             norm_x = (x-running_mean)/jt.sqrt(running_var+self.eps)
+        if not self.affine:
+            return norm_x
         w = self.weight.broadcast(x, [0])
         b = self.bias.broadcast(x, [0])
         return norm_x * w + b
 
 class InstanceNorm2d(Module):
-    def __init__(self, num_features, eps=1e-05, momentum=0.1, affine=None, is_train=True, sync=True):
-        assert affine == None
+    def __init__(self, num_features, eps=1e-05, momentum=0.1, affine=True, is_train=True, sync=True):
         self.sync = sync
         self.num_features = num_features
         self.is_train = is_train
         self.eps = eps
         self.momentum = momentum
-        self.weight = init.constant((num_features,), "float32", 1.0)
-        self.bias = init.constant((num_features,), "float32", 0.0)
+        self.affine = affine
+        if self.affine:
+            self.weight = init.constant((num_features,), "float32", 1.0)
+            self.bias = init.constant((num_features,), "float32", 0.0)
 
     def execute(self, x):
         xmean = jt.mean(x, dims=[2,3], keepdims=1)
@@ -375,18 +381,21 @@ class InstanceNorm2d(Module):
 
         xvar = jt.maximum(x2mean-xmean*xmean, 0)
         norm_x = (x-xmean)/jt.sqrt(xvar+self.eps)
+        if not self.affine:
+            return norm_x
         w = self.weight.broadcast(x, [0,2,3])
         b = self.bias.broadcast(x, [0,2,3])
         return norm_x * w + b
 
 class GroupNorm(Module):
-    def __init__(self, num_groups, num_channels, eps=1e-05, affine=None, is_train=True):
-        assert affine == None
+    def __init__(self, num_groups, num_channels, eps=1e-05, affine=True, is_train=True):
         self.num_groups = num_groups
         self.num_channels = num_channels
         self.eps = eps
-        self.weight = init.constant((num_channels,), "float32", 1.0)
-        self.bias = init.constant((num_channels,), "float32", 0.0)
+        self.affine  = affine
+        if self.affine:
+            self.weight = init.constant((num_channels,), "float32", 1.0)
+            self.bias = init.constant((num_channels,), "float32", 0.0)
 
     def execute(self, x):
         N,C,H,W = x.shape
@@ -397,6 +406,8 @@ class GroupNorm(Module):
         x2mean = jt.mean(x*x, dims=[2,3], keepdims=1)
         xvar = jt.maximum(x2mean-xmean*xmean, 0)
         norm_x = (x-xmean)/jt.sqrt(xvar+self.eps)
+        if not self.affine:
+            return norm_x.reshape((N,C,H,W))
         w = self.weight.reshape((1,self.num_groups,C//self.num_groups,1))
         b = self.bias.reshape((1,self.num_groups,C//self.num_groups,1))
         return (norm_x * w + b).reshape((N,C,H,W))
