7a5244f12d1e577d336c7a68657e2016669f3a42
zhouwy19 <zhouwy1998@foxmail.com>
2020-09-25 15:40:04 +0800

    refine pow

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -330,6 +330,12 @@ Var.masked_fill = masked_fill
 def sqr(x): return x*x
 Var.sqr = sqr
 
+def pow(x, y):
+    if isinstance(y, (ori_int, ori_float)) and y == 2:
+        return x.sqr()
+    return core.ops.pow(x, y)
+Var.pow = Var.__pow__ = pow
+
 def argmax(x, dim, keepdims:bool=False):
     return x.arg_reduce("max", dim, keepdims)
 Var.argmax = argmax
@@ -817,6 +823,7 @@ Var.peek = lambda x: f"{x.dtype}{x.shape}"
 
 
 ori_int = int
+ori_float = float
 
 int = int32
 Var.int = Var.int32
