50e0a125376b2707e460f43b1602f66a0185885d
Dun Liang <randonlang@gmail.com>
2020-10-14 17:58:36 +0800

    slice start==stop

--- extern/cuda/nccl/src/nccl_warper.cc
+++ extern/cuda/nccl/src/nccl_warper.cc
@@ -27,9 +27,6 @@ nccl_initer() {
     int device_count = get_device_count();
     if (!device_count) return;
     if (!inside_mpi) return;
-    if (mpi_world_rank == 0)
-        checkCudaErrors(ncclGetUniqueId(&id));
-    MPI_CHECK(MPI_Bcast((void *)&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
     if (mpi_local_rank >= device_count)
         LOGf << "mpi_local_rank(">>mpi_local_rank>>") is larger than device_count("
             >>device_count>>")";
@@ -39,6 +36,9 @@ nccl_initer() {
     event_queue.run_sync([]() {
         checkCudaErrors(cudaSetDevice(nccl_device_id));
     });
+    if (mpi_world_rank == 0)
+        checkCudaErrors(ncclGetUniqueId(&id));
+    MPI_CHECK(MPI_Bcast((void *)&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
     checkCudaErrors(ncclCommInitRank(&comm, mpi_world_size, id, mpi_world_rank));
 }
 
