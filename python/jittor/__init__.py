aa09164bb22603c909231543ffcdda4a96d435dc
Dun Liang <randonlang@gmail.com>
2020-11-24 09:52:37 +0800

    parallel compiler fix

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -7,7 +7,7 @@
 # This file is subject to the terms and conditions defined in
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
-__version__ = '1.2.1.1'
+__version__ = '1.2.1.2'
 from . import lock
 with lock.lock_scope():
     ori_int = int
