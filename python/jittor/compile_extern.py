6e3daf8c8b02bc106b640d64872bf7f73950fa6b
Dun Liang <randonlang@gmail.com>
2020-11-26 23:08:59 +0800

    cub include for cuda11

--- python/jittor/compile_extern.py
+++ python/jittor/compile_extern.py
@@ -100,8 +100,12 @@ def install_cub(root_folder):
 def setup_cub():
     from pathlib import Path
     cub_path = os.path.join(str(Path.home()), ".cache", "jittor", "cub")
-    cub_home = install_cub(cub_path)
-    setup_cuda_lib("cub", link=False, extra_flags=f"-I{cub_home}")
+    cuda_version = int(get_version(nvcc_path)[1:-1].split('.')[0])
+    extra_flags = ""
+    if cuda_version < 11:
+        cub_home = install_cub(cub_path)
+        extra_flags = f"-I{cub_home}"
+    setup_cuda_lib("cub", link=False, extra_flags=extra_flags)
 
 def setup_cuda_extern():
     if not has_cuda: return
