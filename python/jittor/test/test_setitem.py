7ba6bbdff5920d39715bd76f5ec084cd6ea07d78
周文洋 <zhouwy1998@foxmail.com>
2020-11-27 17:42:22 +0800

    update

--- python/jittor/test/test_setitem.py
+++ python/jittor/test/test_setitem.py
@@ -12,8 +12,77 @@ skip_this_test = False
 
 @unittest.skipIf(skip_this_test, "No Torch found")
 class TestSetitem(unittest.TestCase):
+    def test_setitem(self):
+        arr0 = jt.random((4,2,2))
+        data0 = jt.ones((2,2))
+        arr0[1] = data0
+        arr0.sync()
+        data0.data[0,0] = 0
+        assert arr0[1,0,0] == 0
+
+        arr00 = jt.random((4,2,2))
+        data00 = jt.ones((2,2))
+        # share memory will fail if d has an edge to other nodes.
+        tmp = data00 + 1
+        arr00[1] = data00
+        arr00.sync()
+        data00.data[0,0] = 0
+        assert arr00[1,0,0] == 0
+
+        arr1 = jt.random((4,2,2))
+        data1 = jt.zeros((2,2))
+        arr1[3,:,0:2] = data1
+        arr1.sync()
+        data1.data[0,0] = 1
+        assert arr1[3,0,0] == 1
+
+        arr21 = jt.ones((2,2))
+        arr22 = jt.ones((2,2)) * 2
+        arr2 = jt.contrib.concat([arr21, arr22], dim=0)
+        arr2.sync()
+        arr21.data[0,0] = 3
+        arr22.data[0,0] = 4
+        assert arr2[0,0] == 3
+        assert arr2[2,0] == 4
+
     def test_getitem(self):
-        # test getitem for float32/float64/bool/int8/int32
+        # test for different slice type
+        arr0 = jt.random((4,3))
+        arr0_res = arr0[2,:]
+        arr0_res.data[1] = 1
+        assert arr0[2,1] == 1
+
+        arr1 = jt.array([1,2,3,4])
+        arr1_res = arr1[None]
+        arr1_res.data[0,2] = -1
+        assert arr1[2] == -1
+
+        arr2 = jt.array([1,2,3,4])
+        arr2_res = arr2[...]
+        arr2_res.data[2] = -1
+        assert arr2[2] == -1
+
+        arr3 = jt.array([1,2,3,4])
+        arr3_res = arr3[3]
+        arr3_res.data[0] = -1
+        assert arr3[3] == -1
+
+        arr4 = jt.random((4,2,3,3))
+        arr4_res = arr4[...,:,:]
+        arr4_res.data[0,0,1,1] = 1
+        assert arr4[0,0,1,1] == 1
+
+        arr5 = jt.random((4,2,3,3))
+        arr5_res = arr5[1:3,:,:,:]
+        arr5_res.data[1,0,1,1] = 1
+        assert arr5[2,0,1,1] == 1
+
+        arr6 = jt.random((4,2,3,3))
+        arr6_res = arr6[1]
+        arr6_res.data[0,1,1] = 1
+        assert arr6[1,0,1,1] == 1
+
+        # test for different data type (float32/float64/bool/int8/int32)
         arr_float32 = jt.random((4,2,3))
         arr_float32_res = arr_float32[1:3,:,:]
         arr_float32_res.data[0,0,0] = 1
