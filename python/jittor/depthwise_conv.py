59a92a0a3c36155cc5c5b7f84ad89e036853bfdd
cxjyxx_me <498731903@qq.com>
2020-12-07 22:48:34 +0800

    fix

--- python/jittor/depthwise_conv.py
+++ python/jittor/depthwise_conv.py
@@ -1,3 +1,12 @@
+# ***************************************************************
+# Copyright (c) 2020 Jittor. Authors:
+#     Guoye Yang <498731903@qq.com>
+#     Dun Liang <randonlang@gmail.com>.
+#
+# All Rights Reserved.
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
+# ***************************************************************
 import jittor as jt
 from jittor import init
 from jittor import nn
