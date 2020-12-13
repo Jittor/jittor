1857e83f99d411a0251afd0cc12dc6b855b38b4b
Dun Liang <randonlang@gmail.com>
2020-12-13 22:13:08 +0800

    add bash server

--- script/converter_server.sh
+++ script/converter_server.sh
@@ -5,5 +5,10 @@ RUN python3.7 -m pip install flask
 RUN apt update && apt install git -y
 EOF
 
-sudo docker build --tag jittor/converter_server -f /tmp/converter_server.dockerfile .
-sudo docker run --rm jittor/converter_server bash -c "python3.7 -m pip install -U git+https://github.com/Jittor/jittor.git && python3.7 -m jittor.utils.converter_server"
+docker build --tag jittor/converter_server -f /tmp/converter_server.dockerfile .
+
+# docker run --rm -it -m 16g --cpus=8 -p 0.0.0.0:5000:5000  jittor/converter_server bash -c "python3.7 -m pip install -U git+https://github.com/Jittor/jittor.git && python3.7 -m jittor.utils.converter_server"
+while true; do
+    timeout 24h docker run --rm -it -m 16g --cpus=8 -p 0.0.0.0:5000:5000  jittor/converter_server bash -c "python3.7 -m pip install -U git+https://github.com/Jittor/jittor.git && python3.7 -m jittor.utils.converter_server"
+    sleep 10
+done
