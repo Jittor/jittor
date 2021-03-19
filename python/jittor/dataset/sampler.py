5c6c160aeccb00c27a3cc8ec056a64af0439024e
Exusial <2247838039@qq.com>
2021-03-19 10:10:40 +0800

    fix subset,change jittor op to np op.

--- python/jittor/dataset/sampler.py
+++ python/jittor/dataset/sampler.py
@@ -3,66 +3,70 @@ from .dataset import Dataset
 import numpy as np
 from PIL import Image
 
+
 class Sampler():
-    def __init__(self,data_source):
+    def __init__(self, data_source):
         self.data_source = data_source
-    
+
     def __iter__(self):
         pass
-    
+
     def __len__(self):
         pass
 
+
 class SequentialSampler(Sampler):
-    def __init__(self,data_source):
+    def __init__(self, data_source):
         self.data_source = data_source
-    
+
     def __iter__(self):
         return iter(range(len(self.data_source)))
-    
+
     def __len__(self):
         return len(self.data_source)
 
 
 class RandomSampler(Sampler):
-    def __init__(self,data_source,replacement=False, num_samples=None):
+    def __init__(self, data_source, replacement=False, num_samples=None):
         self.data_source = data_source
         self.rep = replacement
         self._num_samples = num_samples
-    
+
     @property
     def num_samples(self):
         if self._num_samples is None:
             return len(self.data_source)
         return self._num_samples
-    
+
     def __len__(self):
         return self.num_samples
-    
+
     def __iter__(self):
         n = len(self.data_source)
         if self.rep:
-            return iter(np.random.randint(low=0, high=n, size=(self.num_samples,), dtype=np.int64).tolist()) 
-        return iter(jt.randperm(n).numpy().tolist())
+            return iter(np.random.randint(low=0, high=n, size=(self.num_samples,), dtype=np.int64).tolist())
+        return iter(np.random.permutation(n).tolist())
 
 
 class SubsetRandomSampler(Sampler):
-    def __init__(self,indices):
-        self.indices = indices
-    
-    def __iter__(self):        
-        return (self.indices[jt.to_int(i)] for i in jt.randperm((len(self.indices))))
-    
+    def __init__(self, data_source, indice):
+        self.data_source = data_source
+        self.indices = indice
+        assert indice[0] >= 0 and indice[1] < data_source.total_len and indice[0] < indice[1]
+
+    def __iter__(self):
+        return (self.data_source[i + self.indices[0]] for i in np.random.permutation(self.indices[1] - self.indices[0]))
+
     def __len__(self):
-        return len(self.indices)
+        return self.indices[1] - self.indices[0]
 
 
 class BatchSampler(Sampler):
-    def __init__(self,sampler,batch_size,drop_last):
+    def __init__(self, sampler, batch_size, drop_last):
         self.sampler = sampler
         self.batch_size = batch_size
         self.drop_last = drop_last
-    
+
     def __iter__(self):
         batch = []
         for idx in self.sampler:
@@ -72,14 +76,9 @@ class BatchSampler(Sampler):
                 batch = []
         if len(batch) > 0 and not self.drop_last:
             yield batch
-    
+
     def __len__(self):
         if self.drop_last:
             return len(self.sampler) // self.batch_size
         else:
             return (len(self.sampler) + self.batch_size - 1) // self.batch_size
-            
-
-
-
-  
--- python/jittor/test/test_sampler.py
+++ python/jittor/test/test_sampler.py
@@ -4,41 +4,43 @@ from PIL import Image
 import numpy as np
 import unittest
 
-test_img = np.random.normal(size=(40,3,100,100))
+test_img = np.random.normal(size=(40, 1, 2, 2))
+
+
 class TestSamplerDataset(Dataset):
     def __init__(self):
         super().__init__()
-        self.set_attrs(total_len=40,batch_size=1)
+        self.set_attrs(total_len=40, batch_size=1)
 
-    def __getitem__(self,idx):
-        return test_img[idx:(idx+1),...]
+    def __getitem__(self, idx):
+        return test_img[idx:(idx + 1), ...]
+
 
 testdataset = TestSamplerDataset()
+
 
 class TestSampler(unittest.TestCase):
     def test_sequential_sampler(self):
         seqsampler = SequentialSampler(testdataset)
         assert len(seqsampler) == 40
-        for idx,batch in enumerate(seqsampler):
+        for idx, batch in enumerate(seqsampler):
             assert idx == batch
-        
+
     def test_random_sampler(self):
         randomsampler = RandomSampler(testdataset)
         assert len(randomsampler) == 40
-    
+
     def test_subset_random_sampler(self):
-        testdataset.set_subset(20,30)
-        subsetsampler = SubsetRandomSampler(testdataset)
+        subsetsampler = SubsetRandomSampler(testdataset, (20, 30))
         assert len(subsetsampler) == 10
 
     def test_batch_sampler(self):
-        testdataset.subset = None
         seqforbatch = SequentialSampler(testdataset)
-        batchsampler = BatchSampler(seqforbatch,4,drop_last=False)
+        batchsampler = BatchSampler(seqforbatch, 4, drop_last=False)
         assert len(batchsampler) == 10
         for batch in batchsampler:
             assert len(batch) == 4
-  
+
 
 if __name__ == "__main__":
     unittest.main()
