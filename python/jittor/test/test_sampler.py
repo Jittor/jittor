fa62b3055078a10e7fcc26fb65261dfa114e7b19
Dun Liang <randonlang@gmail.com>
2021-03-23 18:56:50 +0800

    add sampler hook in dataset

--- python/jittor/test/test_sampler.py
+++ python/jittor/test/test_sampler.py
@@ -4,7 +4,6 @@ from PIL import Image
 import numpy as np
 import unittest
 
-test_img = np.random.normal(size=(40, 1, 2, 2))
 
 
 class TestSamplerDataset(Dataset):
@@ -13,28 +12,42 @@ class TestSamplerDataset(Dataset):
         self.set_attrs(total_len=40, batch_size=1)
 
     def __getitem__(self, idx):
-        return test_img[idx:(idx + 1), ...]
-
-
-testdataset = TestSamplerDataset()
+        return idx**2
 
 
 class TestSampler(unittest.TestCase):
     def test_sequential_sampler(self):
+        testdataset = TestSamplerDataset()
         seqsampler = SequentialSampler(testdataset)
         assert len(seqsampler) == 40
         for idx, batch in enumerate(seqsampler):
             assert idx == batch
+        for i, data in enumerate(testdataset):
+            assert data.item() == i**2
 
     def test_random_sampler(self):
+        testdataset = TestSamplerDataset()
         randomsampler = RandomSampler(testdataset)
         assert len(randomsampler) == 40
+        diff = 0
+        for i, data in enumerate(testdataset):
+            diff += data.item() == i**2
+        assert diff < 10
 
     def test_subset_random_sampler(self):
+        testdataset = TestSamplerDataset()
         subsetsampler = SubsetRandomSampler(testdataset, (20, 30))
         assert len(subsetsampler) == 10
+        s = 0
+        for i, data in enumerate(testdataset):
+            s += data.item()
+        s2 = 0 
+        for i in range(20,30):
+            s2 += i**2
+        assert s == s2, (s, s2)
 
     def test_batch_sampler(self):
+        testdataset = TestSamplerDataset()
         seqforbatch = SequentialSampler(testdataset)
         batchsampler = BatchSampler(seqforbatch, 4, drop_last=False)
         assert len(batchsampler) == 10
