e8c9da96f1c68f9301abda36e0946517c82d039b
Dun Liang <randonlang@gmail.com>
2021-04-09 12:49:14 +0800

    polish gcc error

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -8,7 +8,7 @@
 # This file is subject to the terms and conditions defined in
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
-__version__ = '1.2.2.58'
+__version__ = '1.2.2.59'
 from . import lock
 with lock.lock_scope():
     ori_int = int
