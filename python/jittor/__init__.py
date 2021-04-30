1e65e183bb5433868a8d1326955d12b8f40591a5
Dun Liang <randonlang@gmail.com>
2021-04-30 19:22:39 +0800

    optimize getitem

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -8,7 +8,7 @@
 # This file is subject to the terms and conditions defined in
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
-__version__ = '1.2.2.63'
+__version__ = '1.2.2.64'
 from . import lock
 with lock.lock_scope():
     ori_int = int
