56e0d831fe4266b3429f005df4449eb9ab42cf00
Dun Liang <randonlang@gmail.com>
2021-05-07 17:34:33 +0800

    add normal, log_prob,entropy ,kl_divergence

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.2.2.68'
+__version__ = '1.2.2.69'
 from . import lock
 with lock.lock_scope():
     ori_int = int
