fd7d68e6aa387bde151cb3a20c6f8b04d171a4f1
Dun Liang <randonlang@gmail.com>
2021-06-05 10:59:06 +0800

    add /usr/lib as cuda lib search path

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.2.3.17'
+__version__ = '1.2.3.18'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
