3427af48978966dabff0b1f73e4a3a220f9e7076
Dun Liang <randonlang@gmail.com>
2021-06-30 17:38:22 +0800

    fix resnet test

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.2.3.48'
+__version__ = '1.2.3.49'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
