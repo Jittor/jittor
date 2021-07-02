e12b4261291f02d9cca3d089863d6fd6c6ded755
Dun Liang <randonlang@gmail.com>
2021-07-02 14:47:18 +0800

    add lock for dl_open

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.2.3.53'
+__version__ = '1.2.3.54'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
