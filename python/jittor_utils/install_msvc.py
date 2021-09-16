63b6f8a5a82927313e0871e0db4238c7d5712902
Dun Liang <randonlang@gmail.com>
2021-09-16 13:51:28 +0800

    polish initialize order

--- python/jittor_utils/install_msvc.py
+++ python/jittor_utils/install_msvc.py
@@ -8,7 +8,7 @@ def install(path):
     LOG.i("Installing MSVC...")
     filename = "msvc.zip"
     url = "https://cg.cs.tsinghua.edu.cn/jittor/assets/" + filename
-    md5sum = "13d420e5919e5ec81155fe923b3d1a07"
+    md5sum = "0fd71436c034808649b24baf28998ccc"
     download_url_to_local(url, filename, path, md5sum)
     fullname = os.path.join(path, filename)
     import zipfile
