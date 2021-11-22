1e1fe66a30b682355589f6ccbe7fd46039a93960
Dun Liang <randonlang@gmail.com>
2021-11-22 13:17:31 +0800

    update version

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.1.19'
+__version__ = '1.3.1.20'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
