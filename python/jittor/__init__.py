6bc17cb99c7e537fa89769e69f53a74e3dbb435c
Dun Liang <randonlang@gmail.com>
2021-12-06 12:21:02 +0800

    polish interpolate

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.1.21'
+__version__ = '1.3.1.22'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
