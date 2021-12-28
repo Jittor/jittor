ef8b42645aad3cc902d8dbc2a100c95192a21c29
Dun Liang <randonlang@gmail.com>
2021-12-28 12:59:43 +0800

    add disable lock flags

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.1.29'
+__version__ = '1.3.1.30'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
