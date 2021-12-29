d6b834920326f2f2fd40d54e6bd7a3647343baf1
Dun Liang <randonlang@gmail.com>
2021-12-29 17:50:20 +0800

    polish reindex fuse

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.1.30'
+__version__ = '1.3.1.31'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
