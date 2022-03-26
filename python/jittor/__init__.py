dbc85d1415da82d2415443b9c055e50823c1bc76
Dun Liang <randonlang@gmail.com>
2022-03-26 15:51:47 +0800

    add amp alias

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.1.54'
+__version__ = '1.3.1.55'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
