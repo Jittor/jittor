33463caf5617248a533d15c2d82f61d60ae68558
Dun Liang <randonlang@gmail.com>
2022-04-22 13:51:06 +0800

    polish mem info

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.2.8'
+__version__ = '1.3.2.9'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
--- python/jittor/src/mem/mem_info.cc
+++ python/jittor/src/mem/mem_info.cc
@@ -191,7 +191,7 @@ void display_memory_info(const char* fileline, bool dump_var, bool red_color) {
         bool gpu_overflow = (double)gpu_total>(double)mem_info.total_cuda_ram*0.95;
         bool cpu_overflow = (double)cpu_total>(double)mem_info.total_cpu_ram*0.95;
         // cpu total too small, not possible
-        if (cpu_total < 100000)
+        if (mem_info.total_cpu_ram < 100000)
             cpu_overflow = false;
         if(gpu_overflow || cpu_overflow) {
             double used = gpu_overflow ? (double)gpu_total : (double)cpu_total;
