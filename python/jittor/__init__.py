0bb901ed9ae8afd685852c3b948d1315a51904ac
Dun Liang <randonlang@gmail.com>
2022-04-22 12:49:18 +0800

    polish mem info

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.2.7'
+__version__ = '1.3.2.8'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
