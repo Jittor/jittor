deeb607167688fe0a68fa1e5bfd0b6712c42df88
Dun Liang <randonlang@gmail.com>
2022-04-23 16:17:14 +0800

    remove cupy require for cumprod

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.3.4'
+__version__ = '1.3.3.5'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
