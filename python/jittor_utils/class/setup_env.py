3bd646d5b90ad5470c4bb56fab52db25364f46b7
cxjyxx_me <498731903@qq.com>
2022-05-09 09:04:51 +0800

    support multi gpu

--- python/jittor_utils/class/setup_env.py
+++ python/jittor_utils/class/setup_env.py
@@ -75,21 +75,26 @@ def setup(n):
             run_cmd('mkdir -p ' + path)
     js.dump(user_info, open(student_files_dir+"/user_info.json", "w"))
 
-def start(n):
+def start(n, n_gpu):
     assert os.path.exists(student_files_dir+'/user_info.json')
     user_info = js.load(open(student_files_dir+'/user_info.json', 'r'))
     for i in range(len(user_info)):
         id = i % n
+        ids = ''
+        for j in range(n_gpu):
+            if j > 0:
+                ids+=','
+            ids += str((i * n_gpu + j) % n)
         u = user_info[i]
         print('START', i, '/', len(user_info))
-        assert 0 == run_cmd(f'docker run -itd --shm-size=8g --network host --name {u["name"]} -v {u["path"]}:/root --gpus "device={id}" jittor/jittor-cuda bash')
+        assert 0 == run_cmd(f'docker run -itd --shm-size=8g --network host --name {u["name"]} -v {u["path"]}:/root --gpus \'"device={ids}"\' jittor/jittor-cuda bash')
         # assert 0 == run_cmd(f'docker exec -it {u["name"]} bash -c \'apt update && apt install openssh-server -y\'')
         assert 0 == run_cmd(f'docker cp {cwd}/setup.py {u["name"]}:/etc/ssh/setup.py')
         assert 0 == run_cmd(f'docker cp {cwd}/motd {u["name"]}:/etc/motd')
         assert 0 == run_cmd(f'docker exec -it {u["name"]} python3.7 /etc/ssh/setup.py passwd {u["passwd"]}')
         assert 0 == run_cmd(f'docker exec -it {u["name"]} python3.7 /etc/ssh/setup.py ssh {u["port"]}')
         assert 0 == run_cmd(f'docker exec -it {u["name"]} python3.7 -m pip install jittor -U')
-        assert 0 == run_cmd(f'docker exec -it {u["name"]} python3.7 -m jittor.test.test_core')
+        assert 0 == run_cmd(f'docker exec -it {u["name"]} python3.7 -m jittor.test.test_example')
 
 def stop():
     assert os.path.exists(student_files_dir+'/user_info.json')
@@ -107,9 +112,9 @@ def report():
         u = user_info[i]
         print(f"ssh -p {u['port']} root@{hostname} # passwd: {u['passwd']}")
 
-def restart(n):
+def restart(n, n_gpu):
     stop()
-    start(n)
+    start(n, n_gpu)
 
 args = sys.argv[1:]
 if (args[0] == 'setup'):
@@ -119,17 +124,27 @@ if (args[0] == 'setup'):
     assert(n < 999)
     setup(n)
 elif (args[0] == 'start'):
-    assert(len(args) == 2)
+    assert(len(args) in [2,3])
     assert(type(eval(args[1])) == int)
     n = int(args[1])
-    start(n)
+    if len(args) == 3:
+        assert(type(eval(args[2])) == int)
+        n_gpu = int(args[2])
+    else:
+        n_gpu=1
+    start(n, n_gpu)
 elif (args[0] == 'stop'):
     stop()
 elif (args[0] == 'restart'):
-    assert(len(args) == 2)
+    assert(len(args) in [2,3])
     assert(type(eval(args[1])) == int)
     n = int(args[1])
-    restart(n)
+    if len(args) == 3:
+        assert(type(eval(args[2])) == int)
+        n_gpu = int(args[2])
+    else:
+        n_gpu=1
+    restart(n, n_gpu)
 elif (args[0] == 'report'):
     report()
 else:
