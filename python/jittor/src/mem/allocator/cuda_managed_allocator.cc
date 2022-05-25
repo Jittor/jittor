a011b8a7ed9c5f1b2387ec5c6a7ae79bbd1a749f
zhouwy19 <zhouwy1998@foxmail.com>
2022-05-25 12:19:26 +0800

    polish unified memory

--- python/jittor/src/mem/allocator/cuda_managed_allocator.cc
+++ python/jittor/src/mem/allocator/cuda_managed_allocator.cc
@@ -12,7 +12,7 @@
 namespace jittor {
 
 CudaManagedAllocator cuda_managed_allocator;
-DEFINE_FLAG(int, use_cuda_managed_allocator, 1, "Enable cuda_managed_allocator");
+DEFINE_FLAG(int, use_cuda_managed_allocator, 0, "Enable cuda_managed_allocator");
 EXTERN_LIB bool no_cuda_error_when_free;
 
 const char* CudaManagedAllocator::name() const {return "cuda_managed";}
