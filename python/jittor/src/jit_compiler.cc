ec9b6eb7a40d193b3fb3c5f3d1e21b4cf4880a99
Exusial <bilibili39@163.com>
2022-06-17 17:48:46 +0800

    split if.

--- python/jittor/src/jit_compiler.cc
+++ python/jittor/src/jit_compiler.cc
@@ -211,7 +211,11 @@ jit_op_entry_t compile(const string& jit_key, const string& src, const bool is_c
     LOGvv << "Compile op" << jit_key;
     // compiler do not allowed filename too long
     CHECK(cc_path.size());
-    string jit_src_path = Op::get_filename_from_jit_key(jit_key, is_cuda_op && (nvcc_flags.find("-dc") != string::npos || extra_flags.find("-dc") != string::npos)?".cu":".cc");
+    string jit_src_path;
+    if(is_cuda_op && (nvcc_flags.find("-dc") != string::npos || extra_flags.find("-dc") != string::npos))
+        jit_src_path = Op::get_filename_from_jit_key(jit_key, ".cu");
+    else
+        jit_src_path = Op::get_filename_from_jit_key(jit_key, ".cc");
     string* src2 = (string*)&src;
     string* extra_flags2 = (string*)&extra_flags;
     JPU(op_compiler(jit_src_path, *src2, is_cuda_op, *extra_flags2));
