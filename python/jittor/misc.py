d8801ae116796ce87050b116a42c491110951954
Exusial <bilibili39@163.com>
2022-09-22 16:29:05 +0800

    add atan2.

--- python/jittor/misc.py
+++ python/jittor/misc.py
@@ -1981,3 +1981,10 @@ jt.Var.scatter_add_ = scatter_add_
 
 def lerp(start, end, weight, out=None):
     return start + weight * (end - start)
+
+def atan2(input, other, out=None):
+    atan_res = jt.atan(input / other)
+    atan_res[jt.bitwise_and(input > 0, other < 0)] += np.pi
+    atan_res[jt.bitwise_and(input < 0, other < 0)] -= np.pi
+    return atan_res
+jt.atan2 = atan2
