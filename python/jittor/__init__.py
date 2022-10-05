9a5e7ea6f5d5711194400144801b7b449f26e3b0
Dun Liang <randonlang@gmail.com>
2022-10-05 16:43:00 +0800

    code op support more than 10 args

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.5.16'
+__version__ = '1.3.5.17'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
