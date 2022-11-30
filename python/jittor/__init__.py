e014f4f25c82f76d493a884f894bfa452dd5f637
Dun Liang <randonlang@gmail.com>
2022-11-30 11:05:21 +0800

    polish cuda env setup

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.5.38'
+__version__ = '1.3.5.39'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
