42771fe77dc1acbd5c83a8cb49a4d3a1d8a74b96
lzhengning <lzhengning@gmail.com>
2023-03-31 19:43:09 +0800

    rollback version

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.6.16'
+__version__ = '1.3.6.15'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
