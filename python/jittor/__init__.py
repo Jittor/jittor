ba9b18eb5e17cb1c387531361da2d85dbf81c61b
Dun Liang <randonlang@gmail.com>
2023-05-01 16:06:42 +0800

    update version to 1.3.7.15

--- python/jittor/__init__.py
+++ python/jittor/__init__.py
@@ -9,7 +9,7 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # ***************************************************************
 
-__version__ = '1.3.7.14'
+__version__ = '1.3.7.15'
 from jittor_utils import lock
 with lock.lock_scope():
     ori_int = int
