b849de1052052f6dc327588c9d13cc852f363421
CHEN Xinsheng <chen.xs.gm@gmail.com>
2024-11-05 12:46:00 +0800

    fix `nonzero`

--- python/jittor/extern/acl/acl_compiler.py
+++ python/jittor/extern/acl/acl_compiler.py
@@ -974,49 +974,15 @@ def change_function():
             super(NonzeroACL, self).__init__()
 
         def execute(self, x):
-            """
-            A temporary implementation, very slow.
-
-            Beacuse output shape is unknown before this op is executed, we temporarily set it to full size, i.e. `(x.numel(), x.ndim)` and then use binary search in post-processing.
-
-            Time (input shape = (2,2,3)): 
-                ACL: 3.7822842597961426 ms
-                CPU: 0.06308555603027344 ms
-                Numpy (`np.where(x)`): 0.0017642974853515625 ms
-            """
-
             attr_code = f"""
             op.jt_name = "nonzero";
             """
+            nonzero_cnt = (x != 0.0).sum().item()
+            
             result = acl_cmd("Nonzero", [x],
                              output_dtypes=['int64'],
-                             output_shapes=[(x.numel(), x.ndim)], # NOTE: shape is unknown before this op is executed
+                             output_shapes=[(nonzero_cnt, x.ndim)],
                              attr_code=attr_code)[0]
-
-            def get_first_element(tensor):
-                if tensor.numel() == 0:
-                    raise ValueError("Tensor is empty")
-                while tensor.ndim > 1:
-                    tensor = tensor[0]
-                return tensor[0].item()
-
-            def truncate_nonzero_elements(tensor, begin=0):
-                low, high = begin, tensor.size(0) - 1
-
-                while low <= high:
-                    mid = (low + high) // 2
-                    if tensor[mid].sum() > 0:
-                        low = mid + 1
-                    else:
-                        high = mid - 1
-
-                truncated_tensor = tensor[:(high + 1)]
-                return truncated_tensor
-
-            if get_first_element(x):
-                result = truncate_nonzero_elements(result, begin=1)
-            else:
-                result = truncate_nonzero_elements(result, begin=0)
 
             return result
 
@@ -1024,8 +990,6 @@ def change_function():
             return grad_output
 
     def nonzero_acl(x):
-        # import numpy as np
-        # return jt.array(np.argwhere(x.numpy()))
         return NonzeroACL()(x)
 
     class FloorIntACL(Function):
