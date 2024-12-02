85f724b6313786ee4f36c188e123d3d7a98cd491
Exusial <phy22@mails.tsinghua.edu.cn>
2024-12-02 17:53:42 +0800

    fix bug

--- python/jittor/extern/acl/aclops/binary_op.h
+++ python/jittor/extern/acl/aclops/binary_op.h
@@ -88,7 +88,9 @@ namespace jittor
 
             ret = it->second.executeFunc(workspaceAddr, workspaceSize, executor, aclstream);
             CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("%s: aclnnxxx failed. ERROR: %d\n", name.c_str(), ret); return);
-            // syncRun();
+
+            syncRun();
+
             aclDestroyScalar(alpha);
             return;
         }
--- python/jittor/extern/acl/aclops/reduce_op.h
+++ python/jittor/extern/acl/aclops/reduce_op.h
@@ -14,11 +14,27 @@ namespace jittor
 
     protected:
         bool is_group_op = false;
-        void executeOp(std::unordered_map<string, AclOpFunctions>::iterator &it)
+        ReduceAttr *attr;
+        aclIntArray *dim;
+        bool keepdims;
+        void setupOutputDesc() override
         {
-            auto attr = dynamic_cast<ReduceAttr *>(op_attr.get());
-            aclIntArray *dim = aclCreateIntArray(attr->axes.data(), attr->axes.size());
-            bool keepdims = attr->keepdims;
+
+            auto output_num = out_.size();
+
+            for (int output_idx = 0; output_idx < output_num; output_idx++)
+            {
+                std::vector<int64_t> shape;
+                for (int j = 0; j < out_[output_idx]->shape.size(); j++)
+                {
+                    shape.push_back(out_[output_idx]->shape[j]);
+                }
+                outputShapes.push_back(shape);
+            }
+
+            attr = dynamic_cast<ReduceAttr *>(op_attr.get());
+            dim = aclCreateIntArray(attr->axes.data(), attr->axes.size());
+            keepdims = attr->keepdims;
 
             if (op_idx < 13)
             {
@@ -26,6 +42,16 @@ namespace jittor
                     outputShapes[0] = {};
             }
 
+            for (int idx = 0; idx < output_num; idx++)
+            {
+                outputTensors.push_back(nullptr);
+                auto ret = CreateAclTensor(outputShapes[idx], out_[idx]->mem_ptr, out_[idx]->size, get_dtype(out_[idx]->dtype()), &outputTensors[idx], use_nchw);
+                CHECK_RET(ret == ACL_SUCCESS, return);
+            }
+        }
+
+        void executeOp(std::unordered_map<string, AclOpFunctions>::iterator &it)
+        {
             switch (op_idx)
             {
             case 9:
@@ -78,7 +104,7 @@ namespace jittor
                 exit(-1);
             }
             }
-            // syncRun();
+            syncRun();
             return;
         }
     };
