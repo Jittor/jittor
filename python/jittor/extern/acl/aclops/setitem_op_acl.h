86331a8d8f541ed0851f83e5a49e55c04e3d8c24
Exusial <phy22@mails.tsinghua.edu.cn>
2024-12-06 11:21:55 +0800

    add setitem & getitem op

---
+++ python/jittor/extern/acl/aclops/setitem_op_acl.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "utils.h"
+#include "base_op.h"
+
+namespace jittor
+{
+    class InplaceMaskedScatterOpRunner : public BaseOpRunner
+    {
+
+    protected:
+        void executeOp(std::unordered_map<string, AclOpFunctions>::iterator &it) override;
+    public:
+        InplaceMaskedScatterOpRunner();
+    };
+
+    class IndexPutImplOpRunner : public BaseOpRunner
+    {
+
+    protected:
+        void executeOp(std::unordered_map<string, AclOpFunctions>::iterator &it) override;
+    public:
+        IndexPutImplOpRunner();
+    };
+}
