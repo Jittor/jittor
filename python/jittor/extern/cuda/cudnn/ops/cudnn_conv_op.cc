ceabd84cfc4965a8d2b29f7232114b46888228a1
张仪 <zhangyi@cscg-hw00>
2026-09-04 12:37:34 +0800

    [2.19] 迁移 cudnn conv 权重 rank 前置

--- python/jittor/extern/cuda/cudnn/ops/cudnn_conv_op.cc
+++ python/jittor/extern/cuda/cudnn/ops/cudnn_conv_op.cc
@@ -61,7 +61,7 @@ CudnnConvOp::CudnnConvOp(Var* x, Var* w, int strideh, int stridew, int paddingh,
 
 void CudnnConvOp::infer_shape() {
     USER_CHECKop(x->shape.size(),==,4);
-    ASSERTop(w->shape.size(),==,4);
+    USER_CHECKop(w->shape.size(),==,4);
     int xn, xc, xh, xw, wh, ww, wci, wco, yn, yc, yh, yw;
     get_shape(x, "abcd", xformat, xn, xc, xh, xw);
     get_shape(w, "oihw", wformat, wco, wci, wh, ww);
