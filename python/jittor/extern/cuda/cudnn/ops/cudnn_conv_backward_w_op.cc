1e3bab6e2a79ba93a887e47cca5a2587860820ac
张仪 <zhangyi@cscg-hw00>
2026-09-04 13:04:16 +0800

    [2.19] 迁移 cudnn conv backward w 输入 rank 前置

--- python/jittor/extern/cuda/cudnn/ops/cudnn_conv_backward_w_op.cc
+++ python/jittor/extern/cuda/cudnn/ops/cudnn_conv_backward_w_op.cc
@@ -60,7 +60,7 @@ CudnnConvBackwardWOp::CudnnConvBackwardWOp(Var* x, Var* dy, int kh, int kw, int 
 }
 
 void CudnnConvBackwardWOp::infer_shape() {
-    ASSERTop(x->shape.size(),==,4);
+    USER_CHECKop(x->shape.size(),==,4);
     ASSERTop(dy->shape.size(),==,4);
     int xn, xc, xh, xw, wh, ww, wci, wco, yn, yc, yh, yw;
     get_shape(x, "abcd", xformat, xn, xc, xh, xw);
